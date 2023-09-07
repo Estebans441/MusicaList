0157fe7efacf9e0167f34440d251e7e53f2a02d7
Esteban Salazar <salazaraesteban@javeriana.edu.co>
2023-09-07 12:29:40 -0500

    intial commit

---
+++ src/test/java/co/edu/javeriana/musicalist/MusicaListApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.musicalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicaListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
