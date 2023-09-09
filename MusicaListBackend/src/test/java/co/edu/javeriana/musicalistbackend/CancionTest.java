8b6f4b49b689fc1794523cdc787a990a5fab4b30
Estebans441 <salazaraesteban@javeriana.edu.co>
2023-09-08 20:35:43 -0500

    feat-cancion-test

--- MusicaListBackend/src/test/java/co/edu/javeriana/musicalistbackend/CancionTest.java
+++ MusicaListBackend/src/test/java/co/edu/javeriana/musicalistbackend/CancionTest.java
@@ -1,4 +1,96 @@
 package co.edu.javeriana.musicalistbackend;
 
+import co.edu.javeriana.musicalistbackend.model.Administrador;
+import co.edu.javeriana.musicalistbackend.model.Cancion;
+import co.edu.javeriana.musicalistbackend.repository.AdministradorRepository;
+import co.edu.javeriana.musicalistbackend.repository.CancionRepository;
+import co.edu.javeriana.musicalistbackend.repository.GeneroMusicalRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@SpringBootTest
+@Transactional
 public class CancionTest {
+    @Autowired
+    CancionRepository cancionRepository;
+
+    @Autowired
+    AdministradorRepository administradorRepository;
+
+    @Autowired
+    GeneroMusicalRepository generoMusicalRepository;
+
+    @Test
+    void crearCancion() {
+        Optional<Administrador> administradorOptional = administradorRepository.findById(1);
+        Administrador administrador = null;
+        Cancion cancion;
+        if (administradorOptional.isPresent())
+            administrador = administradorOptional.get();
+
+        cancion = new Cancion("Something real", "Post Malone", 270, administrador, generoMusicalRepository.findById(1).get());
+
+        cancionRepository.save(cancion);
+        assert (true);
+    }
+
+    @Test
+    void findCancionById(){
+        Cancion cancion = new Cancion();
+        Optional<Cancion> cancionOptional = cancionRepository.findById(1);
+        if(cancionOptional.isPresent())
+            cancion = cancionOptional.get();
+
+        System.out.println("Nombre de la cancion: " + cancion.getNombre());
+        System.out.println("Genero: " + cancion.getGeneroMusical().getNombreGenero());
+
+        assert(true);
+    }
+
+    @Test
+    void findCancionByNombre(){
+        Cancion cancion = new Cancion();
+        Optional<Cancion> cancionOptional = cancionRepository.findByNombre("Shape of you");
+        if(cancionOptional.isPresent())
+            cancion = cancionOptional.get();
+
+        System.out.println("Nombre de la cancion: " + cancion.getNombre());
+        System.out.println("Genero: " + cancion.getGeneroMusical().getNombreGenero());
+
+        assert(true);
+    }
+
+    @Test
+    void findAllCanciones(){
+        ArrayList<Cancion> canciones = (ArrayList<Cancion>) cancionRepository.findAll();
+        for(Cancion cancion : canciones){
+            System.out.println("Nombre de la cancion: " + cancion.getNombre());
+            System.out.println("Genero: " + cancion.getGeneroMusical().getNombreGenero());
+        }
+        assert(true);
+    }
+
+    @Test
+    void actualizarCancion(){
+        Cancion cancion = new Cancion();
+        Optional<Cancion> cancionOptional = cancionRepository.findById(1);
+        if(cancionOptional.isPresent())
+            cancion = cancionOptional.get();
+
+        cancion.setDuracionSeg(120);
+        cancionRepository.save(cancion);
+
+        assert(true);
+    }
+
+     @Test
+    void eliminarCancion(){
+        administradorRepository.deleteById(1);
+        assert(true);
+     }
 }
