70f76f0d6b678f1a606f9b278249a477b71e3fbd
Estebans441 <salazaraesteban@javeriana.edu.co>
2023-09-09 13:36:33 -0500

    cancion-genero-getId-controllers

--- MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/CancionController.java
+++ MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/CancionController.java
@@ -1,4 +1,21 @@
 package co.edu.javeriana.musicalistbackend.controller;
 
+import co.edu.javeriana.musicalistbackend.model.Cancion;
+import co.edu.javeriana.musicalistbackend.repository.CancionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/musicalist/api/cancion")
 public class CancionController {
+    @Autowired
+    private CancionRepository cancionRepository;
+
+    @GetMapping("/{id}")
+    public Cancion getAdministradorId(@PathVariable Integer id){
+        return cancionRepository.findById(id).orElse(null);
+    }
 }
--- MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/GeneroController.java
+++ MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/GeneroController.java
@@ -1,4 +1,21 @@
 package co.edu.javeriana.musicalistbackend.controller;
 
+import co.edu.javeriana.musicalistbackend.model.GeneroMusical;
+import co.edu.javeriana.musicalistbackend.repository.GeneroMusicalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/musicalist/api/genero")
 public class GeneroController {
+    @Autowired
+    GeneroMusicalRepository generoMusicalRepository;
+
+    @GetMapping("/{id}")
+    public GeneroMusical getAdministradorId(@PathVariable Integer id){
+        return generoMusicalRepository.findById(id).orElse(null);
+    }
 }
