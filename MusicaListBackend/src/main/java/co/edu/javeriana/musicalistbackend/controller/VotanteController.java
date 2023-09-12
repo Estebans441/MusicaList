4a48ed8bd926d1764e4787e06c0628bf8bbdf9a1
Estebans441 <salazaraesteban@javeriana.edu.co>
2023-09-11 19:53:53 -0500

    feat-voto-cancion

--- MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/VotanteController.java
+++ MusicaListBackend/src/main/java/co/edu/javeriana/musicalistbackend/controller/VotanteController.java
@@ -1,17 +1,23 @@
 package co.edu.javeriana.musicalistbackend.controller;
 
+import co.edu.javeriana.musicalistbackend.model.Cancion;
 import co.edu.javeriana.musicalistbackend.model.Votante;
+import co.edu.javeriana.musicalistbackend.repository.CancionRepository;
 import co.edu.javeriana.musicalistbackend.repository.VotanteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/musicalist/api/votante")
 public class VotanteController {
     @Autowired
     VotanteRepository votanteRepository;
+
+    @Autowired
+    CancionRepository cancionRepository;
 
     // Create -> POST
     @CrossOrigin
@@ -27,5 +33,29 @@ public class VotanteController {
     @GetMapping("{id}")
     public Votante getVotanteID (@PathVariable Integer id){
         return votanteRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/votar/{idVotante}-{idCancion}")
+    public Boolean votarCancion(@PathVariable Integer idVotante, @PathVariable Integer idCancion){
+        Optional<Votante> votanteOptional = votanteRepository.findById(idVotante);
+        Optional<Cancion> cancionOptional = cancionRepository.findById(idCancion);
+        if(votanteOptional.isEmpty() || cancionOptional.isEmpty())
+            return false;
+        Votante votante = votanteOptional.get();
+        votante.getCancionesVotadas().add(cancionOptional.get());
+        votanteRepository.save(votante);
+        return true;
+    }
+
+    @PutMapping("/eliminar-voto/{idVotante}-{idCancion}")
+    public Boolean eliminarVotoCancion(@PathVariable Integer idVotante, @PathVariable Integer idCancion){
+        Optional<Votante> votanteOptional = votanteRepository.findById(idVotante);
+        Optional<Cancion> cancionOptional = cancionRepository.findById(idCancion);
+        if(votanteOptional.isEmpty() || cancionOptional.isEmpty())
+            return false;
+        Votante votante = votanteOptional.get();
+        votante.getCancionesVotadas().remove(cancionOptional.get());
+        votanteRepository.save(votante);
+        return true;
     }
 }
