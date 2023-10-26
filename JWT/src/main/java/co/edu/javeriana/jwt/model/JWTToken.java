9101dec652ec17d8147a981d1c4f56ee586cc032
Estebans441 <salazaraesteban@javeriana.edu.co>
2023-10-26 08:16:16 -0500

    JWT

---
+++ JWT/src/main/java/co/edu/javeriana/jwt/model/JWTToken.java
@@ -0,0 +1,20 @@
+package co.edu.javeriana.jwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Generated
+@AllArgsConstructor
+public class JWTToken {
+
+    @Generated
+    private String token;
+    @Generated
+    private String prefijo;
+
+
+}
