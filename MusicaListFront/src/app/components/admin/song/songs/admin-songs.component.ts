c6775166668b55c35f4f204003864e224e063696
Estebans441 <salazaraesteban@javeriana.edu.co>
2023-11-11 15:41:37 -0500

    feat-permission-alert

--- MusicaListFront/src/app/components/admin/song/songs/admin-songs.component.ts
+++ MusicaListFront/src/app/components/admin/song/songs/admin-songs.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {CancionService} from "../../../../services/cancion.service";
 import {Cancion} from "../../../../models/entities/cancion.model";
@@ -7,16 +7,19 @@ import {GeneroMusical} from "../../../../models/entities/generoMusical.model";
 import {MatDialog} from "@angular/material/dialog";
 import {AdminAddSongComponent} from "../add-song/admin-add-song.component";
 import {AdminEditSongComponent} from "../edit-song/admin-edit-song.component";
+import {CookieService} from "ngx-cookie-service";
+import {AxiosError} from "axios";
 
 @Component({
   selector: 'app-admin-songs',
   templateUrl: './admin-songs.component.html',
   styleUrls: ['./admin-songs.component.css']
 })
-export class AdminSongsComponent {
+export class AdminSongsComponent implements OnInit {
   id: number
   canciones: Cancion[] = [];
   genero = ""
+  cookieService = inject(CookieService)
 
   constructor(private route: ActivatedRoute, private cancionService: CancionService, private generoService: GeneroMusicalService, public dialog: MatDialog) {
     this.id = -1;
@@ -34,51 +37,88 @@ export class AdminSongsComponent {
   }
 
   agregar() {
-    const dialogRef = this.dialog.open(AdminAddSongComponent, {
-      height: '350px',
-      width: '400px',
-    });
+    if (this.checkToken()) {
+      const dialogRef = this.dialog.open(AdminAddSongComponent, {
+        height: '350px',
+        width: '400px',
+      });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        let cancion: Cancion = result['cancion'];
-        cancion.generoMusical.idGenero = this.id
-        this.cancionService.createCancion(cancion).subscribe(res => {
-          this.generoService.getGeneroMusicalById(this.id).subscribe((genero: GeneroMusical) => {
-              this.canciones = genero.canciones.sort((a, b) => b.numeroVotos - a.numeroVotos);
-              this.genero = genero.nombreGenero
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          let cancion: Cancion = result['cancion'];
+          cancion.generoMusical.idGenero = this.id
+          this.cancionService.createCancion(cancion).subscribe({
+            next: res => {
+              this.generoService.getGeneroMusicalById(this.id).subscribe((genero: GeneroMusical) => {
+                  this.canciones = genero.canciones.sort((a, b) => b.numeroVotos - a.numeroVotos);
+                  this.genero = genero.nombreGenero
+                }
+              )
+            },
+            error: (error: AxiosError) => {
+              if (error.response && error.response.status === 403)
+                alert("No tienes permisos para realizar esta acción");
+              else
+                alert("Error desconocido");
             }
-          )
-        })
-      }
-    })
+          })
+        }
+      })
+    }
   }
 
   editar(id: number, cancion: Cancion) {
-    const dialogRef = this.dialog.open(AdminEditSongComponent, {
-      height: '400px',
-      width: '400px',
-      data: {cancion: cancion, idGenero:this.id}
-    });
+    if (this.checkToken()) {
+      const dialogRef = this.dialog.open(AdminEditSongComponent, {
+        height: '400px',
+        width: '400px',
+        data: {cancion: cancion, idGenero: this.id}
+      });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        let cancion: Cancion = result['cancion'];
-        this.cancionService.updateCancionById(id, cancion).subscribe(res => {
-          this.generoService.getGeneroMusicalById(this.id).subscribe((genero: GeneroMusical) => {
-              this.canciones = genero.canciones.sort((a, b) => b.numeroVotos - a.numeroVotos);
-              this.genero = genero.nombreGenero
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+          let cancion: Cancion = result['cancion'];
+          this.cancionService.updateCancionById(id, cancion).subscribe({
+            next: res => {
+              this.generoService.getGeneroMusicalById(this.id).subscribe((genero: GeneroMusical) => {
+                  this.canciones = genero.canciones.sort((a, b) => b.numeroVotos - a.numeroVotos);
+                  this.genero = genero.nombreGenero
+                }
+              )
+            },
+            error: (error: AxiosError) => {
+              if (error.response && error.response.status === 403)
+                alert("No tienes permisos para realizar esta acción");
+              else
+                alert("Error desconocido");
             }
-          )
-        })
-      }
-    })
+          })
+        }
+      })
+    }
   }
 
-
   eliminarCancion(id: number) {
-    this.cancionService.deleteCancionById(id).subscribe((cancion: Cancion) => {
-      this.canciones = this.canciones.filter((cancion: Cancion) => cancion.idCancion != id)
-    })
+    if (this.checkToken()) {
+      this.cancionService.deleteCancionById(id).subscribe({
+        next: (cancion: Cancion) => {
+          this.canciones = this.canciones.filter((cancion: Cancion) => cancion.idCancion != id)
+        },
+        error: (error: AxiosError) => {
+          if (error.response && error.response.status === 403)
+            alert("No tienes permisos para realizar esta acción");
+          else
+            alert("Error desconocido");
+        }
+      })
+    }
+  }
+
+  checkToken() {
+    if (!this.cookieService.check('JWT-token')) {
+      alert("Es necesario iniciar sesion para realizar esta accion")
+      return false;
+    }
+    return true;
   }
 }
